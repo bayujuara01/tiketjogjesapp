9860577096cad3dab306a876b1e29cf6ee362899
bayujuara01 <ariefyantobayu@gmail.com>
2019-12-09 01:31:14 +0700

    Restructurin Constraint User Interface

--- app/src/main/java/id/co/bubui/tiketsayaapp/RegisterTwoActivity.kt
+++ app/src/main/java/id/co/bubui/tiketsayaapp/RegisterTwoActivity.kt
@@ -1,29 +1,146 @@
 package id.co.bubui.tiketsayaapp
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
+import android.webkit.MimeTypeMap
+import android.widget.*
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+
 
 class RegisterTwoActivity : AppCompatActivity() {
+
+    private lateinit var btnBack: LinearLayout
+    private lateinit var btnContinue: Button
+    private lateinit var imgPicPhotoUser: ImageView
+    private lateinit var btnAddPhoto: Button
+    private lateinit var edtNamaLengkap: EditText
+    private lateinit var edtBio: EditText
+
+    private lateinit var reference: DatabaseReference
+    private lateinit var storage: StorageReference
+
+    private lateinit var photo_location: Uri
+    private var PHOTO_MAX: Int = 1
+
+    private var USERNAME_KEY = "username_key"
+    private var username_key = ""
+    private var username_key_new = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_two)
 
-        val btn_back = findViewById<LinearLayout>(R.id.btn_back)
-        val btn_continue = findViewById<Button>(R.id.btn_continue)
+        getUsernameLocal()
 
-        btn_back.setOnClickListener(View.OnClickListener {
-            intent = Intent(this, RegisterOneActivity::class.java)
-            startActivity(intent)
-        })
+        btnBack = findViewById(R.id.btn_back)
+        btnContinue = findViewById(R.id.btn_continue)
+        btnAddPhoto = findViewById(R.id.btn_add_photo)
+        imgPicPhotoUser = findViewById(R.id.pict_photo_register_user)
+        edtNamaLengkap = findViewById(R.id.edt_nama_lengkap)
+        edtBio = findViewById(R.id.edt_bio)
 
-        btn_continue.setOnClickListener(View.OnClickListener {
-            intent = Intent(this, SuccessRegisterActivity::class.java)
-            startActivity(intent)
-        })
+
+        btnBack.setOnClickListener {
+            val gotoPrevIntent = Intent(this, RegisterOneActivity::class.java)
+            startActivity(gotoPrevIntent)
+        }
+
+        btnContinue.setOnClickListener {
+            // ubah state menjadi loading
+            btnContinue.isEnabled = false
+            btnContinue.setText("Loading ...")
+            //menyimpan ke database
+            reference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("Users")
+                .child(username_key_new)
+
+            storage = FirebaseStorage
+                .getInstance()
+                .getReference()
+                .child("Photousers")
+                .child(username_key_new)
+
+            if(photo_location != null){
+                val mStorageReference: StorageReference = storage.child( System.currentTimeMillis().toString() + "." + getFileExtension(photo_location))
+
+                mStorageReference.putFile(photo_location)
+                    .addOnFailureListener {
+                        //failure upload
+                    }
+                    .addOnSuccessListener {
+
+                        reference.ref.child("nama_lengkap").setValue(edtNamaLengkap.text.toString())
+                        reference.ref.child("bio").setValue(edtBio.text.toString())
+
+                        mStorageReference.downloadUrl.addOnSuccessListener { uri ->
+                            //Toast.makeText(this, "URL : $uri", Toast.LENGTH_LONG).show()
+
+                            reference.ref.child("url_photo_profile").setValue(uri.toString())
+                        }
+                    }.addOnCompleteListener {
+                        val gotoSuccessIntent = Intent(this, SuccessRegisterActivity::class.java)
+                        startActivity(gotoSuccessIntent)
+                    }
+            }
+        }
+
+        btnAddPhoto.setOnClickListener {
+            findPhoto()
+        }
+    }
+
+    //function find photo
+    fun findPhoto(){
+        val pictureIntent = Intent()
+        pictureIntent.setType("image/*")
+        pictureIntent.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(pictureIntent, PHOTO_MAX)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PHOTO_MAX && resultCode == Activity.RESULT_OK && data != null && data.data != null){
+
+            photo_location = data.data!!
+            Picasso.get()
+                .load(photo_location)
+                .centerCrop()
+                .fit()
+                .into(imgPicPhotoUser)
+
+        }
+    }
+
+    fun getFileExtension(uri: Uri): String? {
+        val contentResolver: ContentResolver = contentResolver
+        val mimeTypeMap: MimeTypeMap = MimeTypeMap.getSingleton()
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri))
+    }
+
+    fun getUsernameLocal(){
+        val sharedPreference: SharedPreferences = getSharedPreferences(USERNAME_KEY, Context.MODE_PRIVATE)
+        username_key_new = sharedPreference.getString(username_key, "").toString()
     }
 }
--- app/src/main/java/id/co/bubui/tiketsayaapp/SignInActivity.kt
+++ app/src/main/java/id/co/bubui/tiketsayaapp/SignInActivity.kt
@@ -5,27 +5,73 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.*
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var btnNewAccount: TextView
+    private lateinit var btnSignIn: Button
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+
+    private lateinit var reference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val btn_new_account = findViewById<TextView>(R.id.btn_new_account)
-        val btn_sign_in = findViewById<Button>(R.id.btn_sign_in)
+        btnNewAccount = findViewById(R.id.btn_new_account)
+        btnSignIn = findViewById(R.id.btn_sign_in)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
 
         //menuju balaman resgister
-        btn_new_account.setOnClickListener(View.OnClickListener {
+        btnNewAccount.setOnClickListener(View.OnClickListener {
             intent = Intent(this, RegisterOneActivity::class.java)
             startActivity(intent)
          })
 
         //menuju dashboard
-        btn_sign_in.setOnClickListener(View.OnClickListener {
-            intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+        btnSignIn.setOnClickListener(View.OnClickListener {
+
+            val dUsername: String = username.text.toString()
+            val dPassword: String = password.text.toString()
+
+            reference = FirebaseDatabase.getInstance()
+                .reference
+                .child("Users")
+                .child(dUsername)
+
+            reference.addListenerForSingleValueEvent(object: ValueEventListener{
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if(dataSnapshot.exists() && dUsername != ""){
+
+//                        Toast.makeText(this@SignInActivity, "Username Ditemukan", Toast.LENGTH_SHORT).show()
+                        //Ambil data dari firebase (password)
+                        val passwordFromFirebase: String = dataSnapshot.child("password").value.toString()
+
+                        //validasi password firebase
+                        if(dPassword == passwordFromFirebase){
+                            //berpindah activity
+                            val gotoHomeIntent = Intent(this@SignInActivity, HomeActivity::class.java)
+                            startActivity(gotoHomeIntent)
+                        } else {
+                            Toast.makeText(this@SignInActivity, "Password Salah", Toast.LENGTH_SHORT).show()
+                        }
+
+                    } else {
+                        Toast.makeText(this@SignInActivity, "Username atau Password Salah", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+
+                }
+            })
+
         })
     }
 
